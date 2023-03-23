f5a22891b8ec8ad97565994fa60a9331a59166ba
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-03-23 15:57:09 +0500

    Втораяя задача

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -5,17 +5,48 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите первое целое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе целое число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a > b)
+// {
+//     Console.WriteLine($"Число {a} больше числа {b}.");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {b} больше числа {a}");
+// }
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Введите первое целое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе целое число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(a > b)
+Console.Write("Введите третье целое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 & num1 > num3)
 {
-    Console.WriteLine($"Число {a} больше числа {b}.");
+    Console.WriteLine($"Максимальное число {num1}");
 }
+
+else if(num2 > num1 & num2 > num3)
+{
+    Console.WriteLine($"Максимальное число {num2}");
+}
+
 else
 {
-    Console.WriteLine($"Число {b} больше числа {a}");
+    Console.WriteLine($"Максимальное число {num3}");
 }
