340b2565f24142a4066154e37212132ffb4b81fe
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-03-23 15:04:03 +0500

    Seminar1-23/03/2023

---
+++ Seminar1/Program.cs
@@ -0,0 +1,15 @@
+﻿// int number = 5;
+// double number2 = 12,4;
+// string word = "my word!";
+// char symbol = 't';
+// bool chek = false; 
+
+// int num1 = 5;
+// int num2 = 7;
+
+// Console.WriteLine($"My numbers are {num1} and {num2} and it is good!");
+
+
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your namber is " + num);
