197c98512db5d40e9340b038285ce05fa2ac54dd
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-03-31 12:37:46 +0500

    Семинар 3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -119,5 +119,86 @@
 
 
 
+// Seminar 3.
+// Задача1. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// void ShowRange(int quadrant)
+// {
+//     if(quadrant == 1) Console.WriteLine("x > 0 and y > 0"); 
+//     else if(quadrant == 2) Console.WriteLine("x < 0 and y > 0");
+//     else if(quadrant == 3) Console.WriteLine("x < 0 and y < 0");
+//     else if(quadrant == 4) Console.WriteLine("x > 0 and x < 0");
+//     else Console.WriteLine("Quadrant doesnt exist");
+// }
+
+// Console.WriteLine("Input a number of quadrant: ");
+// int quadNum = Convert.ToInt32(Console.ReadLine());
+
+// ShowRange(quadNum);
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и
+// выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int Div (int x, int y)
+// {
+//     if (x > 0 && y > 0) return 1;
+//     if (x < 0 && y > 0) return 2;
+//     if (x < 0 && y < 0) return 3;
+//     if (x > 0 && y < 0) return 4;
+    
+//     return -1;
+// }
+
+// Console.Write("Введите координату X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Div(x, y));
+
+
+// Задача 3.Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// double Div (double x1, double y1, double x2, double y2)
+// {
+//     double len_x = x2 - x1;
+//     double len_y = y2 - y1;
+
+//     double len_xy = Math.Sqrt(len_x * len_x + len_y * len_y);
+
+//     return len_xy;
+// }
+
+// Console.Write("Введите координату X1: ");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y1: ");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату X2: ");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y2: ");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Math.Round(Div(x1, y1, x2, y2), 2));
+
+
+// Задача 4. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+// void Div(int num)
+// {
+//     for(int i = 1; i <= num; i++) Console.WriteLine($"{i} - {i*i}");
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Div(num);
+
+
 
 
