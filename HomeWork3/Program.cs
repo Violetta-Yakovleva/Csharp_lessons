41fea5c44580c7827a1c1569468f8b9d4d61c255
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-03-31 14:22:56 +0500

    Задача 1-3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом 
+// (одинаковое при прочтении слева-направо и справа-налево).
+
+// Решение для ПЯТИЗНАЧНОГО числа
+
+// bool Palindrome(int number)
+// {
+//     int a = number / 10000;
+//     int b =  number % 10;
+//     int c = (number / 1000) % 10;
+//     int d = (number / 10) % 10;
+
+//     if(a == b && c == d) return true;
+//     else return false;
+// }
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int div = number / 10000;
+// if(div >= 1 && div<= 9) 
+// {
+//     if(Palindrome(number) == true) 
+//     Console.WriteLine("Число является палиндромом");
+//     else 
+//     Console.WriteLine("Число не является палиндромом");
+// }
+// else Console.WriteLine("Число не является пятизначным, повторите ввод.");
+
+
+// !!!УНИВЕРСАЛЬНОЕ решение для любого числа!!!
+
+
+
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// double Lenght(double x1, double y1, double z1, double x2, double y2, double z2)
+// {
+//     double lenghtX = x2 - x1;
+//     double lenghtY = y2 - y1;
+//     double lengtZ = z2 - z1;
+
+//     double lenghtXYZ = Math.Sqrt(Math.Pow(lenghtX,2) + Math.Pow(lenghtY, 2) + Math.Pow(lengtZ, 2));
+//     return lenghtXYZ;
+// }
+
+// Console.WriteLine("Введите координаты первой точки в трехмерном пространстве");
+// Console.Write("Координата X: ");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Координата Y: ");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Координата Z: ");
+// double z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координаты второй точки в трехмерном пространстве");
+// Console.Write("Координата X: ");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Координата Y: ");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Координата Z: ");
+// double z2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Расстояние между заданными точками равняется: ");
+// Console.Write(Math.Round(Lenght(x1, y1, z1, x2, y2, z2), 2));
+
+
+// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+// void NumThirdPower(int num)
+// {
+//     for(int n = 1; n <= num; n = n + 1)
+//     Console.WriteLine($"{n} - {Math.Pow(n, 3)}"); 
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// NumThirdPower(num);
