350f3ed8a944e1aeba7671f813e9c6be5b2041b7
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-04-20 17:17:01 +0500

    Seminar 9

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1021,3 +1021,66 @@
 
 
 
+// Семинар 9.
+
+// Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// void ShowNums(int num)
+// {
+//     if(num > 1) ShowNums(num - 1);
+//     Console.Write(num + " ");
+// }
+// ShowNums(5);
+
+
+// Задача 2.Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// int SumOfDigits(int num)
+// {
+//     if(num != 0) return SumOfDigits(num / 10) + num % 10;
+//     else return 0;
+// }
+// Console.Write(SumOfDigits(1234));
+
+
+// Задача 3. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.  
+
+// void ShowNums(int m, int n)
+// {
+//     if(m > n) 
+//     {
+//         Console.Write(m + " ");
+//         ShowNums(m - 1, n);
+//     }
+
+//     if(m < n) 
+//     {
+//         ShowNums(m, n - 1);
+//         Console.Write(n + " ");
+//     }
+
+//     if(m == n)
+//     {
+//         Console.Write(n + " ");
+//     }
+// }
+
+// ShowNums(2, 7);
+
+
+
+// Задача 4. Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+// double MathFunc(int a, int b)
+// {
+//     if(b > 0) return MathFunc(a, b - 1) * a;
+
+//     else if (b < 0) return MathFunc(a, b + 1) / a;
+
+//     else return 1;
+
+// }
+
+
+
+
