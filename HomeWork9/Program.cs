a3057f0a9e829dd74cc43edbb97a3ed193781b11
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-04-20 18:11:57 +0500

    HomeWork 9

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -40,7 +40,7 @@
 // {
 //     if(m == 0) 
 //         return n + 1;
-//     else if(n == 0) 
+//     else if(n == 0 && m > 0) 
 //         return Akkerman(m - 1, 1);
 //     else 
 //         return Akkerman(m - 1, Akkerman(m, n - 1));
